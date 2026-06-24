1b70c97a66a7ccaace6d21a4c85a2d31e5fd47b6
Cristea3051 <cristeavictor4@gmail.com>
2026-06-24 19:54:32 +0300

    login to sauce is done the test works vrey good but need to perform all test cases and for this I need to connect test buddy

---
+++ src/test/java/com/tests/SauceDemoTest.java
@@ -0,0 +1,16 @@
+package com.tests;
+
+import com.framework.config.BaseTest;
+import com.pages.SauceDemoPage;
+import org.testng.annotations.Test;
+
+public class SauceDemoTest extends BaseTest {
+
+    @Test
+    public void navigateToSauce(){
+        SauceDemoPage saucePage = new SauceDemoPage(page());
+        saucePage.navigateToSauce();
+        saucePage.loginToSauce("standard_user", "secret_sauce");
+    }
+
+}
